62abdd8f8c6627aecb0934f8149c4727b7759a2d
marufhossainkhan <kmaruf260@gmail.com>
2022-10-17 11:52:55 +0600

    3rd commit

--- js/resp.js
+++ js/resp.js
@@ -4,8 +4,9 @@ rightnav = document.querySelector('.rightnav')
 navlist = document.querySelector('.navlist')
 
 
+
 burger.addEventListener('click', ()=>{
-    navbar.classList.toggle('v-class-res');
-    navlist.classList.toggle('v-class-res');
-    navbar.classList.toggle('navheight-res');
+    rightnav.classList.toggle('v-class-res')
+    navlist.classList.toggle('v-class-res')
+    navbar.classList.toggle('nav-height-res')
 })
